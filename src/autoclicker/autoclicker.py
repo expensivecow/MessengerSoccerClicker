76e6e448843fc698bd9158b149c1f6429aa95d4c
Michael Cao <michael.cao@alumni.ubc.ca>
2019-04-27 16:23:37 -0700

    update to add full screen image, update with templatematching source code

--- src/autoclicker/autoclicker.py
+++ src/autoclicker/autoclicker.py
@@ -1,5 +1,5 @@
 import os
-import pyscreenshot as ig
+import pyautogui
 import cv2
 import numpy as np
 
@@ -12,19 +12,17 @@ def main():
     find_initial_screen(img_path)
 
 def find_initial_screen(img_path):
-    full_screen = ig.grab()
-    full_screen_np = np.array(full_screen.getdata(), dtype='uint8').reshape((full_screen.size[1], full_screen.size[0], 3))
+    full_screen = pyautogui.screenshot(os.path.join(img_path, 'full_screen.png'))
 
+    full_screen_template = cv2.imread(os.path.join(img_path, 'full_screen.png'), 0)
     mobile_screen_template = cv2.imread(os.path.join(img_path, 'screen_without_score.png'), 0)
-
-    method = cv2.TM_CCOEFF_NORMED
-    res = cv2.matchTemplate(full_screen_np, mobile_screen_template, cv2.TM_CCOEFF)
-
-    '''
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
     w, h = mobile_screen_template.shape[::-1]
 
+    method = cv2.TM_SQDIFF_NORMED
+    res = cv2.matchTemplate(full_screen_template, mobile_screen_template, method)
+
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
@@ -33,14 +31,14 @@ def find_initial_screen(img_path):
         top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
 
-    cv2.rectangle(full_screen_np,top_left, bottom_right, 255, 2)
+    cv2.rectangle(full_screen_template,top_left, bottom_right, 255, 2)
 
-    cv2.imshow('hello', full_screen_np)
+    cv2.imshow('hello', full_screen_template)
     while(True):
         if (cv2.waitKey(25) & 0xFF == ord('q')):
             cv2.destroyAllWindows()
             break
     #full_screen.save(os.path.join(img_path, 'full_screen.png'))
-    '''
+
 if __name__ == "__main__":
     main()
